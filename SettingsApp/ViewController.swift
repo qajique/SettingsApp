567578899f95ebc5825732667336593477997610
Kazhymurat <komirzhan@gmail.com>
2022-02-22 14:36:54 +0600

    Initial Commit

---
+++ SettingsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingsApp
+//
+//  Created by Kazhymurat on 22.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
